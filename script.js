4164d3417c4c0c24c885ae420100da0bc08a2ea6
Julian Barker <julian.c.barker@gmail.com>
2022-08-03 15:24:33 -0700

    add javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+function colorScript() {
+    let x = prompt('Provide a hexadecimal color value, please \( ex. e044bb). Or type in random for a random color!',)
+    if (x == 'random') {
+        const a = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        const c = Math.floor(Math.random()*256);
+        const bg = 'rgb(' + a + ',' + b + ',' + c + ')';
+        console.log(bg);
+        document.body.style.backgroundColor = bg;
+    } else {
+        document.body.style.backgroundColor = '#' + x;
+    }
+}
+
+function greet() {
+    const x = document.getElementById('greeting').value;
+    document.getElementById('response').innerHTML = x;
+}
+
+function powers() {
+    let x = document.getElementById('arg1').value;
+    let y = document.getElementById('arg2').value;
+    if (x === '') {
+        x = 0;
+    }
+    if (y === '') {
+        y = 0;
+    }
+    const z = Math.pow(x,y)
+    const ans = x + ' to the ' + y + ' power = ' + z;
+    document.getElementById('answer').innerHTML = ans;
+    document.getElementById('arg1').value = '';
+    document.getElementById('arg2').value = '';
+}
