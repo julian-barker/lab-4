00d96a88fffbc3dfd22a6ac2cb1ceb685cf95f2a
Julian Barker <julian.c.barker@gmail.com>
2022-08-04 13:38:45 -0700

    attempt 5 at fxing link

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function repeatImage() {
     console.log(x);
     images = document.getElementById("images");
     for (let i = 0; i < x; i++) {
-        images.innerHTML += ("<div><img src='lab-4/images/food.jpeg' alt='delicious foods' width='400px'>" + (i+1) + "</div>");
+        images.innerHTML += ("<div><img src='./images/food.jpeg' alt='delicious foods' width='400px'>" + (i+1) + "</div>");
         //console.log('adding image ' + (i+1));
     }
 }
