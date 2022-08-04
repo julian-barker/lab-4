7cd1df02bf3090c2de8a8cbfc6f828235c4fb082
Julian Barker <julian.c.barker@gmail.com>
2022-08-04 13:20:23 -0700

    add wrap to images

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function repeatImage() {
     console.log(x);
     images = document.getElementById("images");
     for (let i = 0; i < x; i++) {
-        images.innerHTML += ("<div><img src=\"/images/food.jpeg\" alt=\"delicious foods\" width=\"300px\">" + (i+1) + "</div>");
-        console.log('adding image ' + (i+1));
+        images.innerHTML += ("<div><img src=\"/images/food.jpeg\" alt=\"delicious foods\" width=\"400px\">" + (i+1) + "</div>");
+        //console.log('adding image ' + (i+1));
     }
 }
 
